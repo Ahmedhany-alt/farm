fd5abf4d8e3a99896dcf390c5538cedc20c15651
mohamedayman1411 <madoayman03@gmail.com>
2026-09-20 18:10:44 +0300

    i did the chicken and some of time codes ,i just have 4 errors, if you can fix them i will be thankful

---
+++ scenes/Characters/Chiken/idlestate.gd
@@ -0,0 +1,38 @@
+extends Node
+
+@export var character: CharacterBody2D
+@export var animation_sprite_2d:AnimatedSprite2D
+@export var idle_state_timer: float=5.0
+
+@onready var idle_state_timeer: Timer
+
+var idle_state_timeout: bool = false
+
+func _ready() -> void:
+	idle_state_timer.wait_time=idle_state_time_interval
+	idle_state_timer.timeout.connect(on idle state_timeout)
+	add_child(idle_state_timer)
+
+func _on_process(_delta : float) -> void:
+	pass
+
+
+func _on_physics_process(_delta : float) -> void:
+	pass
+
+
+func _on_next_transitions() -> void:
+	if idle_state_timeout:
+		transition.emit("walk")
+
+func _on_enter() -> void:
+	animation_sprite_2d.play("idle")
+
+
+	idle_state_timeout = false
+	idle_state_timer.start()
+func _on_exit() -> void:
+	animation_sprite_2d.stop()
+	idle_state_timer.stop()
+func on_idle_state_timeout()  -> void
+	idle_state_timeout = true
--- scripts/state machine/node_state_machine.gd
+++ scripts/state machine/node_state_machine.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 	if initial_node_state:
 		initial_node_state._on_enter()
 		current_node_state = initial_node_state
-
+		current_node_state_name = current_node_state.name.to_lower()
 
 func _process(delta : float) -> void:
 	if current_node_state:
@@ -26,10 +26,8 @@ func _process(delta : float) -> void:
 func _physics_process(delta: float) -> void:
 	if current_node_state:
 		current_node_state._on_physics_process(delta)
-		current_node_state._on_next_transitions()
-		#print("Current State: ", current_node_state_name)
-
-
+		current_node_state._on_next_transitions() 
+		print("Current State: ", current_node_state_name)
 func transition_to(node_state_name : String) -> void:
 	if node_state_name == current_node_state.name.to_lower():
 		return
@@ -44,6 +42,6 @@ func transition_to(node_state_name : String) -> void:
 	
 	new_node_state._on_enter()
 	
-	current_node_state = new_node_state
+	current_node_state = new_node_state 
 	current_node_state_name = current_node_state.name.to_lower()
 	#print("Current State: ", current_node_state_name)
