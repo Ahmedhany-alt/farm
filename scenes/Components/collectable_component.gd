4d1cfaebf28b271312d9e046c4e98cbfd81de3c7
Ahmedhany-alt <ahany65066@gmail.com>
2026-09-21 05:45:44 +0300

    main scene

--- scenes/Components/collectable_component.gd
+++ scenes/Components/collectable_component.gd
@@ -6,7 +6,3 @@ func _on_body_entered(body: Node2D)-> void:
 	if body is Player:
 		print ("collected")
 		get_parent().queue_free()
-
-
-func _on_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
