4cd892c31eca3c33241faa65eaade8901be5ec69
Ahmedhany-alt <ahany65066@gmail.com>
2026-09-22 22:08:55 +0300

    last changes

--- scenes/Components/hit_component.gd
+++ scenes/Components/hit_component.gd
@@ -4,3 +4,7 @@ extends Area2D
 
 @export var current_tool : DataTypes.Tools = DataTypes.Tools.None
 @export var hit_damage : int = 1
+
+
+func _on_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
--- scenes/Components/hurt_component.gd
+++ scenes/Components/hurt_component.gd
@@ -6,5 +6,8 @@ extends Area2D
 signal on_hurt
 
 func _on_area_entered(area: Area2D) -> void:
-	if area is HitComponent:
-		on_hurt.emit(area.damage)
+	var hit_component = area as HitComponent
+	
+	if tool == hit_component.current_tool:
+		on_hurt.emit(hit_component.hit_damage)
+		
